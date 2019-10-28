b2c51eafc94d673b8c507c0942631b008df68cb3
Sara Oliver <s.oliver1@icloud.com>
2019-10-28 17:14:55 +0000

    still trying

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,51 +5,53 @@
     function makeGraphs(error, countrypopulationData)   {
     var ndx = crossfilter(countrypopulationData);
 
-    var Population_Dim = ndx.dimension(function(d){
-            return d.Population;
+         populationDim = ndx.dimension(function(d){
+            return d.population;
         });
         var tradeColors = d3.scale.ordinal()
-            .domain(["Country", "Continent"])
+            .domain(["Country", "Population"])
             .range(["red", "green"]);
 
-    var specific_Country = ndx.dimension(dc.pluck("Population"));
-    var Country_Dim = ndx.dimension(function(d) {
-       return [d.Specific_Country, d.Population];
-    });
-    var specificCountryGroup = specificCountryDim.group();
-
-        var min_Population = Population_dim.bottom(1)[0].Population;
-        var max_Population = Population_dim.top(1)[0].Population;
-
-        var Continent_Dim = ndx.dimension(dc.pluck("Population"));
-        var tot_Continent = ndx.dimension(function(d){
-            return [d.Population, d.Country, d.Continent];
-
-        var Area_dim = ndx.dimension(function(d){
-            return [d.Area, d.Population, d.Country, d.Continent];
+        var country = ndx.dimension(dc.pluck("Population"));
+        countryDim = ndx.dimension(function(d) {
+       return [d.country, d.population];
         });
-        var AreaGroup = AreaDim.group();
-        var Area_chart = dc.scatterPlot("#scatter-plot");
-        Area_chart
+       
+        var countryGroup = countryDim.group();
+        minPopulation = populationDim.bottom(1)[0].population;
+        maxPopulation = populationDim.top(1)[0].population;
+        
+        var area = ndx.dimension(dc.pluck("Population"));
+        areaDim = ndx.dimension(function(d) {
+       return [d.area, d.population];
+        });
+
+        areaDim = ndx.dimension(function(d){
+            return [d.area, d.population, d.country];
+        });
+        var areaGroup = areaDim.group();
+        
+        var area_chart = dc.scatterPlot("#scatter-plot");
+
+        area_chart
             .width(768)
             .height(480)
-            .x(d3.time.scale().domain([min_Population, max_Population]))
+            .x(d3.time.scale().domain([minPopulation, maxPopulation]))
             .brushOn(false)
             .symbolSize(8)
             .clipPadding(10)
             .yAxisLabel("Country")
             .title(function (d) {
-                return d.key[2] + "Country" + d.key[2].Country + "Population" + d.key[2].Population +"Area" + d.key[2].Area;
+                return d.key[2] + "Country" + d.key[2].country + "Population" + d.key[2].population +"Area" + d.key[2].area;
             })
             .colorAccessor(function (d) {
-                return d.key[2].Area;
+                return d.key[2].area;
             })
             .colors(tradeColors)
-            .dimension(Area_dim)
-            .group(Area_group);
+            .dimension(areaDim)
+            .group(areaGroup);
 
       dc.renderAll();
 
-    })
+    }
 
- 
